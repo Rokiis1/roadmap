82795e6ab2acf63260d495555659cdbf7037cb40
Rokas <94966788+Rokiis1@users.noreply.github.com>
2023-07-14 06:54:18 +0000

    add in forIn instructions to solve the task

--- tasks/vanilla JavaScript/Loops and Iterations/4.forIn.js
+++ tasks/vanilla JavaScript/Loops and Iterations/4.forIn.js
@@ -3,24 +3,31 @@
  * Prints the key-value pairs of an object using a for...in loop.
  * @param {Object} obj - The object to iterate over.
  */
-// Hint: what is Object and how to create
+// Hint: 
+// Step 1: Declare and initialize an object.
+// Step 2: Iterate over the object using a for...in loop.
+// Step 3: Within the for...in loop, access each key-value pair and print them.
 
 // 2 Task.
 /**
  * Iterates over an array using a for...in loop and displays the index and value of each element.
  * @param {Array} arr - The array to iterate over.
  */
-// Hint: What is Array and how to create
+// Hint:
+// Step 1: Declare and initialize an array.
+// Step 2: Iterate over the array using a for...in loop.
+// Step 3: Within the for...in loop, access the index and value of each element and display them.
 
 // 3 Task.
 /**
- * Prints the fruits that start with the letter 'a' using a for...in loop.
- * @param {Object} obj - The object containing fruit properties.
- * const fruits = {
- * apple: 'Red',
- * banana: 'Yellow',
- * apricot: 'Orange',
- * avocado: 'Green'
- * };
+ * Calculates the sum of values in an object using a for...in loop.
+ * @param {Object} obj - The input object.
+ * @returns {number} - The sum of all values in the object.
  */
-// Hint: what is hasOwnProperty()
+// Hint:
+// Step 1: We define an object obj with key-value pairs representing numeric values:
+// Step 2: We initialize a variable sum to 0. This variable will hold the sum of all values in the object:
+// Step 3: We use a for...in loop to iterate over the keys in the obj object:
+// Step 4: Inside the loop, for each key, we access the corresponding value in the obj object and add it to the sum variable:
+// Step 5: After the loop finishes, the sum variable holds the sum of all values in the obj object.
+// Step 6: Finally, we log the resulting sum to the console.
