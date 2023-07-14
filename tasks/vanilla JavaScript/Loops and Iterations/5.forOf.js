336367cfedb489dff0dd6c981d9581a45c4496ea
Rokas <94966788+Rokiis1@users.noreply.github.com>
2023-07-14 07:26:29 +0000

    for of add hints

--- tasks/vanilla JavaScript/Loops and Iterations/5.forOf.js
+++ tasks/vanilla JavaScript/Loops and Iterations/5.forOf.js
@@ -4,12 +4,22 @@
  * @param {number[]} numbers - The array of numbers.
  * @returns {number} The sum of the numbers.
  */
+// Hint:
+// Step 1: Declare and initialize the numbers array.
+// Step 2: Declare and initialize the sum variable.
+// Step 3: Begin the for...of loop to iterate over each number in the numbers array.
+// Step 4: Inside the loop, add the current number to the sum variable.
+// Step 6: Output the calculated sum.
 
 // 2 Task.
 /**
  * Iterates over a string using a for...of loop and displays each character on a new line.
  * @param {string} str - The string to iterate over.
  */
+// Hint:
+// Step 1: Declare and initialize the str variable with the string to iterate over.
+// Step 2: Use the for...of loop to iterate over each character in the string.
+// Step 3: Output the current character on a new line.
 
 // 3 Task.
 /**
@@ -17,5 +27,9 @@
  * @param {Object[]} objects - The array of objects.
  * @param {string} property - The specific property to display.
  */
-
+// Hint:
+// Step 1: Declare and initialize an array of objects.
+// Step 2: Declare and initialize a string variable representing the specific property to display.
+// Step 3: Iterate over each object in the objects array using a for...of loop.
+// Step 4: Access the value of the specific property and display it.
   
